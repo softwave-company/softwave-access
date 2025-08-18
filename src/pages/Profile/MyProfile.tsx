85e811824a0258f00f3a4e2758175f8cee758187
Bruno <b.rafaelgon@outlook.com>
2025-08-17 22:36:54 -0300

    style: add shadow

--- src/pages/Profile/MyProfile.tsx
+++ src/pages/Profile/MyProfile.tsx
@@ -3,7 +3,7 @@ import Footer from "../../components/Footer";
 import { useAuth } from "../../context/useAuth";
 import { Link } from "react-router-dom";
 import EditableAvatar from "../../components/EditableAvatar";
-
+import purpleShadow from '../../assets/images/purple-shadow-about.webp'
 import { CalendarClock, ClockFading, UserPen } from "lucide-react";
 import { useState } from "react";
 
@@ -15,25 +15,35 @@ export default function MyProfile() {
     <div className='flex min-h-screen flex-col bg-[#1c1c28]'>
       <Navbar />
       <main className="w-full flex-1">
-        <div className="mt-6 flex flex-col justify-center">
+        <div className="mt-6 flex flex-col items-center relative">
+          <img
+            src={purpleShadow}
+            alt=""
+            className="absolute -top-120 scale-30"
+          />
+
+
+          {/* Avatar */}
           <EditableAvatar />
+
           <p className="text-white text-center font-bold text-3xl mt-4">{user?.nome}</p>
+
           <Link to="/profile/edit" className="flex justify-center items-center">
             <span className="text-blue-300 mt-2">Editar perfil</span>
             <UserPen className="w-4 h-4 mt-2 ml-2 text-blue-300" />
           </Link>
         </div>
 
-        <div className="flex gap-2 mt-4 w-full max-w-sm mx-auto">
+        <div className="flex gap-2 mt-8 w-full max-w-sm mx-auto">
           <button
-            className={`flex-1 flex items-center justify-center gap-2 font-bold py-2 rounded-lg transition duration-300 ${activeTab === "emBreve" ? "bg-purple-500 text-white" : "bg-gray-200"
+            className={`flex-1 cursor-pointer flex items-center justify-center gap-2 font-bold py-2 rounded-lg transition duration-300 ${activeTab === "emBreve" ? "bg-purple-500 text-white" : "bg-gray-200"
               }`}
             onClick={() => setActiveTab("emBreve")}
           >
             <CalendarClock /> Em breve
           </button>
           <button
-            className={`flex-1 flex items-center justify-center gap-2 font-bold py-2 rounded-lg transition duration-300 ${activeTab === "passados" ? "bg-purple-500 text-white" : "bg-gray-200"
+            className={`flex-1 flex cursor-pointer items-center justify-center gap-2 font-bold py-2 rounded-lg transition duration-300 ${activeTab === "passados" ? "bg-purple-500 text-white" : "bg-gray-200"
               }`}
             onClick={() => setActiveTab("passados")}
           >
@@ -42,13 +52,13 @@ export default function MyProfile() {
         </div>
 
         <div className="mt-8">
-          {activeTab === "emBreve" && 
+          {activeTab === "emBreve" &&
             <div>
               <p className="text-white text-center font-bold text-3xl">Nenhum evento encontrado</p>
               <p className="text-white text-center text-md">Procure alguns eventos pelo Access</p>
             </div>
           }
-          {activeTab === "passados" && 
+          {activeTab === "passados" &&
             <div>
               <p className="text-white text-center font-bold text-3xl">Nenhum evento encontrado</p>
               <p className="text-white text-center text-md">Vá em algum evento utilizando Access!</p>
